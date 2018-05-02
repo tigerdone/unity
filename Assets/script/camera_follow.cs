1f45c6fe04ed1e11f082411a10323e863c21b869
tigerdone <3196083633@qq.com>
2018-05-02 19:29:02 +0800

    first

---
+++ Assets/script/camera_follow.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_follow : MonoBehaviour {
+    public Transform hero;
+
+    public float Xdist = 3f;
+    public float Ydist = 3f;
+    public Vector2 minxy;
+    public Vector2 maxxy;
+    public float xsmooth = 5f;
+    public float ysmooth = 5f;
+
+
+    bool IsMoveX()
+    {
+        if(Mathf.Abs(transform.position.x - hero.position.x) > Xdist)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    void trackCam()
+    {
+        float newx = transform.position.x;
+        float newy = transform.position.x;
+        if (IsMoveX())
+        {
+            newx = Mathf.Lerp(transform.position.x, hero.position.x,xsmooth*Time.deltaTime);
+        }
+
+        newx = Mathf.Clamp(newx, minxy.x, maxxy.x);
+        newy = Mathf.Clamp(newy, minxy.y, maxxy.y);
+
+
+        transform.position = new Vector3(newx, newx, newx);
+    }
+
+
+    private float x = 0f;
+    private float y = 0f;
+    private float z = -24.14f;
+    
+
+    void position()
+    {
+        x = hero.position.x;
+        y = hero.position.y;
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+        hero = GameObject.Find("hero").transform;
+        //hero = GameObject.FindGameObjectsWithTag("Player").transform;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector3(hero.position.x, hero.position.y, z);
+        Debug.Log(hero.position.x);
+        Debug.Log(hero.position.y);
+
+
+    }
+
+
+}
+
+
+
+
---
+++ Assets/script/move.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public Rigidbody2D rig;
+    public Transform origin;
+	// Use this for initialization
+	void Start () {
+        mGroundCheck = transform.Find("GroundCheck");
+
+    }
+    public int MaxSpeed = 10;
+    public int Moveforce = 80;
+
+    private bool bFacerright = true;
+    private bool bJump = false;
+    public Transform mGroundCheck;
+
+
+    void FixedUpdate()
+    {
+        float fInput = Input.GetAxis("Horizontal");
+        //float fInput = Input.GetAxis("Vertical");
+
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+        //控制移动
+
+        if(Input.GetKeyDown(KeyCode.Space)&& Physics2D.Raycast(origin.position,Vector2.down,0.1f))
+        {
+            rig.AddForce(new Vector2(0, 500));
+        }
+            
+
+        if (fInput * rigidBody.velocity.x < MaxSpeed)
+        {
+            rigidBody.AddForce(Vector2.right * fInput * Moveforce);
+        }
+
+
+        //限制最大速度
+        if (Mathf.Abs(rigidBody.velocity.x) > MaxSpeed)
+        {
+            rigidBody.velocity = new Vector2(Mathf.Sign(rigidBody.velocity.x) * MaxSpeed, rigidBody.velocity.y);
+        }
+        if (fInput > 0 && !bFacerright)
+        {
+            flip();
+        }
+        if(fInput < 0 && bFacerright)
+        {
+            flip();
+
+        }
+
+
+        //Debug.Log(rigidBody.velocity[0]);
+        //Debug.Log(fInput);
+
+        //if (bJump)
+        //{
+        //    rigidBody.AddForce(new Vector2(0, jumpf))
+        //    bJump = false;
+        //}
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void flip()
+    {
+        bFacerright = !bFacerright;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
+
